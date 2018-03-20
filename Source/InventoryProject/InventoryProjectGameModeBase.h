7df3379ba3989294f57f664a9e35aa96bc8bc01c
marc-abertay <marci.philippe@googlemail.com>
2018-03-20 12:12:40 +0000

    Revert "ignore now working"
    This reverts commit d37ca4f6d9d868aa5b9dab55edcd8b6f76309546.

---
+++ Source/InventoryProject/InventoryProjectGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "InventoryProjectGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class INVENTORYPROJECT_API AInventoryProjectGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
