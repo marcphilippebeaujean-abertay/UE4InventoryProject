8b26317438fec1442ba012f027e1d11d89f795b1
marc-abertay <marci.philippe@googlemail.com>
2018-04-13 00:02:24 +0100

    light of lantern now depracating

--- Source/InventoryProject/LanternItemComponent.cpp
+++ Source/InventoryProject/LanternItemComponent.cpp
@@ -18,13 +18,15 @@ ULanternItemComponent::ULanternItemComponent()
 void ULanternItemComponent::BeginPlay()
 {
 	Super::BeginPlay();
-
+	// Find the light component in the player's components
 	LanternLight = GetOwner()->FindComponentByClass<UPointLightComponent>();
 	if (!LanternLight)
 	{
 		UE_LOG(LogTemp, Error, TEXT("Failed to find lantern spot light!"));
 		return;
 	}
+	
+	CurAttenuation = InitAttenuation;
 
 	// ...
 	
@@ -35,6 +37,25 @@ void ULanternItemComponent::BeginPlay()
 void ULanternItemComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	if(LanternLight)
+	{
+		// Decrement attenuation of light
+		if(CurAttenuation > 0)
+		{
+			CurAttenuation -= (LightDecrementRate * DeltaTime);
+			if(CurAttenuation < 8)
+			{
+				CurAttenuation = 8.0f;
+			}
+			LanternLight->SetAttenuationRadius(CurAttenuation);
+		}
+	}
+	else
+	{
+		UE_LOG(LogTemp, Error, TEXT("Failed to find lantern spot light!"));
+		return;
+	}
 
 	// ...
 }
--- Source/InventoryProject/LanternItemComponent.h
+++ Source/InventoryProject/LanternItemComponent.h
@@ -21,7 +21,6 @@ protected:
 	// Called when the game starts
 	virtual void BeginPlay() override;
 
-
 public:	
 	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
@@ -30,4 +29,12 @@ private:
 	// Reference to player's point light component
 	UPointLightComponent* LanternLight = nullptr;
 	
+	// Speed at which the attenuation of the light decreases, when the lanter has been lit
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Inventory", Meta = (AllowPrivateAccess = "true"))
+	float LightDecrementRate = 1.0f;
+
+	// Variable used to control the attenuation of the light dynamicall
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Inventory", Meta = (AllowPrivateAccess = "true"))
+	float InitAttenuation = 10.0f;
+	float CurAttenuation = 0.0f;
 };
